cc08e0db8dbc7f19cb5904f590270637f3e1b833
Eiji Otsuki <bigoonage0104@gmail.com>
2020-06-25 18:13:24 +0900

    dotinstall web development ~#34

--- basic_php_webdev-master/work/web/index.php
+++ basic_php_webdev-master/work/web/index.php
@@ -2,17 +2,40 @@
 
 require('../app/functions.php');
 
+createToken();
+
+define('FILENAME', '../app/messages.txt');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST')  {
+  validateToken();
+  $message = trim(filter_input(INPUT_POST, 'message'));
+  $message = $message !== '' ? $message : '...';
+  
+  $filename = '../app/messages.txt';
+  $fp = fopen(FILENAME, 'a');
+  fwrite($fp, $message . "\n");
+  fclose($fp);
+
+  header('Location: http://localhost:8080/result.php');
+  exit;
+}
+
+$messages = file(FILENAME, FILE_IGNORE_NEW_LINES);
+
 include('../app/_parts/_header.php');
 
 ?>
 
-<form action="result.php" method="get">
-  <select name="colors[]" multiple>
-    <option value="orange">orange</option>
-    <option value="pink">pink</option>
-    <option value="gold">gold</option>
-  </select>
-  <button>Send</button>
+<ul>
+<?php foreach ($messages as $message): ?>
+  <li><?= h($message); ?></li>
+<?php endforeach; ?>
+</ul>
+
+<form action="" method="post">
+  <input type="text" name="message">
+  <button>Post</button>
+  <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
 </form>
 
 <?php
--- basic_php_webdev-master/work/web/result.php
+++ basic_php_webdev-master/work/web/result.php
@@ -2,16 +2,14 @@
 
 require('../app/functions.php');
 
-// $message = trim(filter_input(INPUT_GET, 'message'));
-// $message = $message !== '' ? $message : '...';
-$colors = filter_input(INPUT_GET, 'colors', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
-$colors = empty($colors) ? 'None selected' : implode(',', $colors);
+
+
 
 include('../app/_parts/_header.php');
 
 ?>
 
-<p><?= h($colors); ?></p>
+<p>Message added!</p>
 <p><a href="index.php">Go back</a></p>
 
 <?php
