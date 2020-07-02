218a1636d99482cf3b04692f9f4a8e79d6ff0ed8
Eiji Otsuki <bigoonage0104@gmail.com>
2020-07-02 19:17:40 +0900

    Update paiza

---
+++ paiza.php
@@ -0,0 +1,22 @@
+<?php
+    
+    // 【入力された値を比べて最大値を算出】
+    // $count = fgets(STDIN);
+    // $max = 0;
+
+    // for ($i = 0; $i < $count; $i++) {
+    //     $number = (int)fgets(STDIN);
+    //     if ($number > $max) {
+    //       $max = $number;
+    //     }
+    // }
+
+    // echo $max;
+    // ------------------------------------
+
+    $count = fgets(STDIN);
+    for ($i = 0; $i < $count; $i++) {
+      echo "paiza ";
+    }
+
+?>
